edcd93aa117876d1f9fe1396fa8c0fb67373064b
Planet <planet27272@hotmail.com>
2025-05-24 10:54:00 +0100

    Fixed v1
    Changed loadFile to loadURL. Leanback now loads successfully.

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const createWindow = () => {
         kiosk: true
     })
     win.webContents.userAgent = "Mozilla/5.0 (SMART-TV; Linux; Tizen 6.0) AppleWebKit/537.36 (KHTML, like Gecko) SamsungBrowser/4.0 Chrome/76.0.3809.146 TV Safari/537.36"
-    win.loadFile('https://youtube.com/tv');
+    win.loadURL('https://youtube.com/tv/');
 }
 app.whenReady().then(() => {
     createWindow()
